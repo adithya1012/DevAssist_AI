5839cd38b7435652f4e71d8215bfe1e18fd2c12a
Prajwal Hosahalli Dayananda <hosap02@pfw.edu>
2024-10-17 15:29:09 -0400

    Refactor prettier configuration and ignore files

--- esbuild.js
+++ esbuild.js
@@ -1,56 +1,58 @@
 const esbuild = require("esbuild");
+const fs = require("fs");
+const path = require("path");
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
 
 /**
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-	name: 'esbuild-problem-matcher',
+	name: "esbuild-problem-matcher",
 
 	setup(build) {
 		build.onStart(() => {
-			console.log('[watch] build started');
+			console.log("[watch] build started");
 		});
 		build.onEnd((result) => {
 			result.errors.forEach(({ text, location }) => {
 				console.error(`✘ [ERROR] ${text}`);
 				console.error(`    ${location.file}:${location.line}:${location.column}:`);
 			});
-			console.log('[watch] build finished');
+			console.log("[watch] build finished");
 		});
 	},
 };
 
+const extensionConfig = {
+	bundle: true,
+	minify: production,
+	sourcemap: !production,
+	logLevel: "silent",
+	plugins: [
+		/* add to the end of plugins array */
+		esbuildProblemMatcherPlugin,
+	],
+	entryPoints: ["src/extension.ts"],
+	format: "cjs",
+	sourcesContent: false,
+	platform: "node",
+	outfile: "dist/extension.js",
+	external: ["vscode"],
+};
+
 async function main() {
-	const ctx = await esbuild.context({
-		entryPoints: [
-			'src/extension.ts'
-		],
-		bundle: true,
-		format: 'cjs',
-		minify: production,
-		sourcemap: !production,
-		sourcesContent: false,
-		platform: 'node',
-		outfile: 'dist/extension.js',
-		external: ['vscode'],
-		logLevel: 'silent',
-		plugins: [
-			/* add to the end of plugins array */
-			esbuildProblemMatcherPlugin,
-		],
-	});
+	const extensionCtx = await esbuild.context(extensionConfig);
 	if (watch) {
-		await ctx.watch();
+		await extensionCtx.watch();
 	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
+		await extensionCtx.rebuild();
+		await extensionCtx.dispose();
 	}
 }
 
-main().catch(e => {
+main().catch((e) => {
 	console.error(e);
 	process.exit(1);
 });
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,92 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { DevAssistProvider } from "./core/webview/DevAssistProvider";
+
+let outputChannel: vscode.OutputChannel;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	outputChannel = vscode.window.createOutputChannel("DevAssist");
+	context.subscriptions.push(outputChannel);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "devassist_ai" is now active!');
+	outputChannel.appendLine("DevAssist AI is now active");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('devassist_ai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		console.log("this is hello world");
-		vscode.window.showInformationMessage('Hello World from devassist_ai!');
-	});
+	const sidebarProvider = new DevAssistProvider(context, outputChannel);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(DevAssistProvider.sideBarId, sidebarProvider, {
+			webviewOptions: {
+				retainContextWhenHidden: true,
+			},
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("devassist_ai.buttonClicked", async () => {
+			outputChannel.appendLine("Button Clicked");
+		})
+	);
+
+	const openDevAssistInNewTab = async () => {
+		outputChannel.appendLine("Opening DevAssist in new tab");
+		// (this example uses webviewProvider activation event which is necessary to deserialize cached webview, but since we use retainContextWhenHidden, we don't need to use that event)
+		// https://github.com/microsoft/vscode-extension-samples/blob/main/webview-sample/src/extension.ts
+		const tabProvider = new DevAssistProvider(context, outputChannel);
+		//const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined
+		const lastCol = Math.max(...vscode.window.visibleTextEditors.map((editor) => editor.viewColumn || 0));
+
+		// Check if there are any visible text editors, otherwise open a new group to the right
+		const hasVisibleEditors = vscode.window.visibleTextEditors.length > 0;
+		if (!hasVisibleEditors) {
+			await vscode.commands.executeCommand("workbench.action.newGroupRight");
+		}
+		const targetCol = hasVisibleEditors ? Math.max(lastCol + 1, 1) : vscode.ViewColumn.Two;
+
+		const panel = vscode.window.createWebviewPanel(DevAssistProvider.tabPanelId, "DevAssist", targetCol, {
+			enableScripts: true,
+			retainContextWhenHidden: true,
+			localResourceRoots: [context.extensionUri],
+		});
+		// TODO: use better svg icon with light and dark variants (see https://stackoverflow.com/questions/58365687/vscode-extension-iconpath)
+
+		panel.iconPath = {
+			light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "robot_panel_light.png"),
+			dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "robot_panel_dark.png"),
+		};
+		tabProvider.resolveWebviewView(panel);
+
+		// Lock the editor group so clicking on files doesn't open them over the panel
+		// await delay(100);
+		await vscode.commands.executeCommand("workbench.action.lockEditorGroup");
+	};
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("devassist_ai.popoutButtonClicked", openDevAssistInNewTab)
+	);
+	context.subscriptions.push(vscode.commands.registerCommand("devassist_ai.openInNewTab", openDevAssistInNewTab));
+
+	const handleUri = async (uri: vscode.Uri) => {
+		const path = uri.path;
+		const query = new URLSearchParams(uri.query.replace(/\+/g, "%2B"));
+		console.log("Handling URI", uri.toString());
+		switch (path) {
+			case "/openrouter": {
+				const code = query.get("code");
+				if (code) {
+					// await visibleProvider.handleOpenRouterCallback(code);
+				}
+				break;
+			}
+			default:
+				break;
+		}
+	};
+	context.subscriptions.push(vscode.window.registerUriHandler({ handleUri }));
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	outputChannel.appendLine("DevAssist AI extension deactivated");
+}
